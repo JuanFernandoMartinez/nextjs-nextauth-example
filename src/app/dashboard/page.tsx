a5057da31e5674ad8964f932737f1b5ba2bb2013
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-06 18:51:34 -0500

    using getServerSession function to analyze google's session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
-export default function DashboardPage(){
+import {getServerSession} from 'next-auth'
+
+export default async function DashboardPage(){
+    const session = await getServerSession()
+    console.log(session)
     return (
         <h1>Dashboard</h1>
     )
