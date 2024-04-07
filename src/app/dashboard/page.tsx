230ea6042ee6d570a989e3c61ad4902b91d056f3
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-06 19:21:43 -0500

    signout button added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,12 @@
 import {getServerSession} from 'next-auth'
-
+import SignOutButton from './SignOutButton'
 export default async function DashboardPage(){
     const session = await getServerSession()
     return (<div>
         <h1>{session?.user?.name}</h1>
         <h3>{session?.user?.email}</h3>
         <img src={session?.user?.image ?? ''} alt="user's image" />
+        <SignOutButton></SignOutButton>
         </div>
     )
 }
