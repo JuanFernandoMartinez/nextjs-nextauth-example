81bda7da1cc464e9da4f24a03a2480360a97b7a5
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-06 19:32:16 -0500

    fitting button style

--- src/app/dashboard/SignOutButton.tsx
+++ src/app/dashboard/SignOutButton.tsx
@@ -4,7 +4,7 @@ import {signOut} from 'next-auth/react'
 
 export default function SignOutButton(){
     return <button
-    className='hover:bg-red-700 rounded-full bg-red-500' 
+    className='hover:bg-red-700 rounded-full bg-red-500 text-white font-bold py-2 px-4 ' 
     onClick={()=>{signOut}}>Cerrar sesión
     </button>
 }
