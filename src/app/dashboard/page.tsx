7efec92d6a967a6144d3e1b397873db22fbfe9de
Fr4nkC0d3 <juan.martinez32@correo.icesi.edu.co>
2024-04-06 19:33:47 -0500

    changed structure tags

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,8 +4,8 @@ export default async function DashboardPage(){
     const session = await getServerSession()
     return (<div className='flex justify-center items-center h-screen'>
         <div className='bg-slate-300 p-10 text-center'>
-        <h1>{session?.user?.name}</h1>
-        <h3>{session?.user?.email}</h3>
+        <h1>Bienvenido {session?.user?.name}</h1>
+        <p>{session?.user?.email}</p>
         <img src={session?.user?.image ?? ''} alt="user's image" />
         <SignOutButton></SignOutButton>
         </div>
